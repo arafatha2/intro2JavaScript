d16bf6eec89479c621b1dd8a4d7abfff4842054a
Arafath Ahmed <144722263+arafatha2@users.noreply.github.com>
2024-03-14 18:26:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction(){
+  
+document.getElementById("greet").style.color="yellow";
+}
+function night() {
+  document.body.style.backgroundColor="darkblue";
+}
+
+function paragraph(){
+  document.getElementById("anything").style.fontFamily="fantasy"
+}
